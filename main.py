8f09ca26beec1cdcf6e394c2c803f987ebfb9d75
Breno Santana <contatobrenosantana@outlook.com>
2023-10-25 23:54:46 -0300

    Início do código

---
+++ main.py
@@ -0,0 +1,17 @@
+def telaInicial():
+    print(
+        """
+        Seja bem-vind ao organizador 50 30 20 
+        """
+    )
+
+
+def cr():
+    return None
+
+
+renda = float(input("Qual é a sua renda mensal?\n"))
+
+cinquentapercent = renda*0.5
+trintapercent = renda*0.3
+vintepercent = renda*0.2
